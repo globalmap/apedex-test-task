054f41342cd934a5a4a39786afb6ecdd506dbd28
Oleksandr <alexander.hanaychuk@gmail.com>
2025-10-06 16:33:55 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <h1 className="title">🌟 Phantom Wallet</h1>
+        <p className="subtitle">Відправка Solana</p>
+      </div>
+    </div>
+
+  )
+}
+
+export default App
